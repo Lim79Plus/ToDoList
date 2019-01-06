376ba31b14d2edb4e4e75e49684282b099c8f849
Lim79Plus <masae.h79@gmail.com>
2019-01-06 12:34:55 +0900

    add .ignore file,lib jquery, basic js logic

---
+++ assets/js/todoList.js
@@ -0,0 +1,43 @@
+//check off spesific todos by 
+// following listener execute only page read,
+// so when you add new todo, the new one do not create listener
+// $("li").click(function () {
+//     $(this).toggleClass("listCompleted");
+// });
+//following code, when every you click whole ul space,
+//create eventListener
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("listCompleted");
+});
+
+//click on X to delete todo
+//following code, do not work for added todo
+// $("span").click(function (event) {
+//     $(this).parent().fadeOut(function () {
+//         $(this).remove();
+//     })
+//     event.stopPropagation();
+// })
+
+$("ul").on("click", "span", function (event) {
+    //in this method 
+    //$(this)== span
+    //$(this).parent() == li
+    $(this).parent().fadeOut(function () {
+        //in here $(this) == li
+        $(this).remove();
+    })
+    //jQuery Method stop anyother listener
+    event.stopPropagation();
+})
+
+//add todo
+$("input[type='text']").keypress(function (event) {
+    //event.which === 13 means hit enter
+    if (event.which === 13) {
+        let newTodoText = $(this).val();
+        $(this).val("");
+        console.log(newTodoText);
+        $("ul").append("<li><span>X </span>" + newTodoText + "</li>");
+    }
+})
